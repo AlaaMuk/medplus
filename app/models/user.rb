4e5c962569d3cd43a2a1d6ddd77bd9245df31ab9
AlaaMuk <alaa.muk@gmail.com>
2015-02-25 23:05:16 -0500

    Added a Provider column, boolean, to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
 	has_secure_password
 	has_many :medications
+	has_many :patients
+	has_many :hcprof 
+	
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150225223620) do
+ActiveRecord::Schema.define(version: 20150226040027) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.boolean  "provider"
   end
 
   add_index "users", ["username"], name: "index_users_on_username", unique: true
