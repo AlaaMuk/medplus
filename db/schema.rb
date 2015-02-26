e59ae4e8ea301e76069d3029e170e1661c531098
Eric Betancourt <eric_betancourt@outlook.com>
2015-02-26 17:58:55 -0500

    added email

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150226041741) do
+ActiveRecord::Schema.define(version: 20150226210932) do
 
   create_table "hcprofs", force: :cascade do |t|
     t.integer  "user_id"
@@ -75,8 +75,10 @@ ActiveRecord::Schema.define(version: 20150226041741) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.boolean  "provider"
+    t.string   "email"
   end
 
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
